bbd0be18ae8546e7d2f6d973e5d925496c8ad16f
Ivan Saldano <ivanms1@gmail.com>
2023-06-17 00:20:03 +0900

    feat: add i18n

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import useTranslation from "next-translate/useTranslation";
+
 function Home() {
+  const { t } = useTranslation("home");
   return (
     <div className="flex h-screen items-center justify-center">
-      <p className="text-6xl text-red-500">hello</p>
+      <p className="text-6xl text-red-500">{t("welcome")}</p>
     </div>
   );
 }
